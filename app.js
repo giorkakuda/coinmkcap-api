f3acc70c4062bb1bf76ea38928ea4981fad29456
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2023-02-08 21:05:53 -0300

    fetch ok

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-let url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-    qString = "?CMC_PRO_API_KEY=" + _0xe548[0] + "&start=1&limit=5&convert=USD";
 
-fetch(url + qString)
-    .then(function(response) {
-        console.log(response);
+
+function getData(){
+    fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=0b273a41-72ec-4cb6-8c21-35a4a6fb12eb")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data) 
+        const name = data.data[0].name
+        console.log(name)
+
+        const quote = data.data[0].quote
+        console.log(quote)
+        document.getElementById('price')
+        .innerHTML = parseFloat(quote.USD.price).toLocaleString('en',{style:'currency',currency: 'USD'})
+    })
+}
+
+getData()
+  
     
-    });
--- app1.js
+++ app1.js
@@ -15,3 +15,11 @@ var _0xe548=["\x30\x62\x32\x37\x33\x61\x34\x31\x2D\x37\x32\x65\x63\x2D\x34\x63\x
 /*
 eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('3 2=1[0]',4,4,'|_0xe548|apikey|let'.split('|'),0,{}))
 */
+
+
+// or use CORS Extension: https://chrome.google.com/webstore/de...
+
+//fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=0b273a41-72ec-4cb6-8c21-35a4a6fb12eb")
+//    .then((response) => response.json())
+ //   .then((data) => console.log(data))  
+  
