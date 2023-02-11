2a98d1112220e355369a458a62cb54f919a5472d
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2023-02-11 16:54:57 -0300

    error handling ok

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 
 
-function getData(){
+function getData(){ /*obfuscate api key*/
     fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=0b273a41-72ec-4cb6-8c21-35a4a6fb12eb")
-    .then((response) => response.json())
+    .then((response) => {
+        if (!response.ok){
+            throw new Error('Too many requests. API call limit exceeded. Please try again next month.');
+        }
+        return response.json();
+    })
     .then((data) => {
         
         const btc = data.data[0].quote
@@ -17,10 +22,16 @@ function getData(){
         document.getElementById('price3')
         .innerHTML = parseFloat(bnb.USD.price).toLocaleString('en',{style:'currency',currency: 'USD'})
 
-        const doge = data.data[8].quote
+        const doge = data.data[8].quote /* data.data.find(coin => coin.symbol === 'DOGE').quote => DIDN'T RETURN*/
         document.getElementById('price4')
         .innerHTML = parseFloat(doge.USD.price).toLocaleString('en',{style:'currency',currency: 'USD'})
     })
+    .catch((error) => {
+        document.getElementById('price').innerHTML = error.message;
+        document.getElementById('price2').innerHTML = error.message;
+        document.getElementById('price3').innerHTML = error.message;
+        document.getElementById('price4').innerHTML = error.message;
+    });
 }
 
 getData()
