f1bd19c7bcfd89129b5b2036f63b56c3c992968a
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2023-02-21 20:15:56 -0300

    well donee

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 
-const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=";
-const apiKey = '0b273a41-72ec-4cb6-8c21-35a4a6fb12eb';
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const apiUrl = proxyUrl + url + apiKey
 
-function getData(){ /*obfuscate api key*/
-    fetch(apiUrl)
-
-    .then((response) => {
-        if (!response.ok){
-            throw new Error('Too many requests. API call limit exceeded. Please try again next month.');
-        }
-        return response.json();
+function getData(){ 
+    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=0b273a41-72ec-4cb6-8c21-35a4a6fb12eb')}`)
+    .then(response => {
+        if (!response.ok) 
+        throw new Error('Too many resquests. API call limit exceeded. Please try again next month.')
+        
+        return response.json()
     })
     .then((data) => {
-        console.log(data);
+        const response = JSON.parse(data.contents); // parsing JSON 
+        console.log(response);
         
-        const btc = data.data.find(coin => coin.symbol === 'BTC').quote
+        const btc = response.data.find(coin => coin.symbol === 'BTC').quote
         document.getElementById('price')
         .innerHTML = parseFloat(btc.USD.price).toLocaleString('en',{style:'currency',currency: 'USD'})
         
-        const eth = data.data.find(coin => coin.symbol === 'ETH').quote
+        const eth = response.data.find(coin => coin.symbol === 'ETH').quote
         document.getElementById('price2')
         .innerHTML = parseFloat(eth.USD.price).toLocaleString('en',{style:'currency',currency: 'USD'})
 
-        const bnb = data.data.find(coin => coin.symbol === 'BNB').quote
+        const bnb = response.data.find(coin => coin.symbol === 'BNB').quote
         document.getElementById('price3')
         .innerHTML = parseFloat(bnb.USD.price).toLocaleString('en',{style:'currency',currency: 'USD'})
 
-        const doge = data.data.find(coin => coin.symbol === 'DOGE').quote 
+        const doge = response.data.find(coin => coin.symbol === 'DOGE').quote 
         document.getElementById('price4')
         .innerHTML = parseFloat(doge.USD.price).toLocaleString('en',{style:'currency',currency: 'USD'})
     })
@@ -41,5 +37,3 @@ function getData(){ /*obfuscate api key*/
 }
 
 getData()
-  
-    
